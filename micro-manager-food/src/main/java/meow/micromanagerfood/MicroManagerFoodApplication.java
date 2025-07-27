f1252add1e90b718bf53aeb075e904875330b753
eliaskrr0 <developer.elias.essadak@gmail.com>
2025-07-27 14:50:11 +0200

    inicio

---
+++ micro-manager-food/src/main/java/meow/micromanagerfood/MicroManagerFoodApplication.java
@@ -0,0 +1,13 @@
+package meow.micromanagerfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroManagerFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroManagerFoodApplication.class, args);
+    }
+
+}
---
+++ micro-manager-food/src/test/java/meow/micromanagerfood/MicroManagerFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package meow.micromanagerfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroManagerFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
