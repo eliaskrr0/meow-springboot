a162ace003009cb36979e596733ed25107179a61
eliaskrr0 <developer.elias.essadak@gmail.com>
2025-08-02 22:27:10 +0200

    inicio

---
+++ micro-manager-recipe/src/main/java/meow/micromanagerrecipe/MicroManagerRecipeApplication.java
@@ -0,0 +1,13 @@
+package meow.micromanagerrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroManagerRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroManagerRecipeApplication.class, args);
+	}
+
+}
