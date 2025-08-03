a1aab85ce4d194993f6ad2d8cf6aa9b660c076b1
eliaskrr0 <developer.elias.essadak@gmail.com>
2025-08-03 11:44:03 +0200

    se agrega modulo para poner en comun los DTOs

---
+++ common-dto/src/main/java/meow/commondto/FoodDTO.java
@@ -0,0 +1,39 @@
+package meow.commondto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoodDTO {
+
+    @Schema(example = "1")
+    private Long idFood;
+
+    @Schema(example = "Manzana")
+    private String name;
+
+    @Schema(example = "Hacendado")
+    private String brand;
+
+    @Schema(example = "gr")
+    private String unitMeasure;
+
+    @Schema(example = "100")
+    private int amount;
+
+    @Schema(example = "1.5")
+    private int proteinAmount;
+
+    @Schema(example = "22")
+    private int carbsAmount;
+
+    @Schema(example = "0")
+    private int fatAmount;
+
+    @Schema(example = "98")
+    private int calories;
+}
